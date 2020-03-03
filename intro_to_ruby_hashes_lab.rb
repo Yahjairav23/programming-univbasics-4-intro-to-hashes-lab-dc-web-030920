195203d757da067139fa54e5731be1b6aea8af88
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-03 20:17:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,9 +38,9 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    hash[key] += 1 
+    hash[key] = hash[key] + 1 
   else
-    hash[key] = 1 
+    hash[key] = key => 1 
   end
   return hash[key]
 end
